602e1bad10305a559f3253f261d9c89a4da672af
Sahil Patel <patelsahil9768@gmail.com>
2026-06-30 21:09:06 +0530

    feat: add Angular environments and bind API base URL dynamically

--- src/app/services/interview-api.service.ts
+++ src/app/services/interview-api.service.ts
@@ -1,12 +1,13 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class InterviewApiService {
-  private readonly baseUrl = 'http://localhost:3000/api/interview';
+  private readonly baseUrl = environment.apiUrl;
 
   constructor(private http: HttpClient) {}
 
